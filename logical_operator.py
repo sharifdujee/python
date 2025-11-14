9aae24bc2eb2dcca362e3b8e4f18452ad05d3a9c
sharifdujee <sharifdu44@gmail.com>
2025-11-14 23:03:51 +0600

    logical and ternary operator

---
+++ logical_operator.py
@@ -0,0 +1,21 @@
+num1 = 20
+num2 = 10
+num3 = 8
+
+# and or not
+
+'''if num1 > num2 and num1 > num3:
+    print(f"{num1} is greater")
+elif num2 > num1 and num2 > num3:
+    print(f"{num2} is greater")
+
+else:
+    print(f"{num3} is greater")'''
+ch = "a"
+if ch == "a" or ch == "e" or ch == "i" or ch == "o" or ch == "u":
+    print("Vowel")
+else:
+    print("Constants")
+
+
+
---
+++ ternary_operator.py
@@ -0,0 +1,10 @@
+num1 = 10
+num2 = 5
+
+'''if num1 > num2:
+    print(f"the {num1} is greater than {num2}")
+
+else:
+    print(f"{num2} is greater than {num1}")'''
+
+print(num1 if num1 > num2 else num2)
