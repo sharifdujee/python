0ba394f9a0f413df822f4cbaef4a698c1420be54
sharifdujee <sharifdu44@gmail.com>
2025-11-15 21:26:19 +0600

    gpa calculation

--- grade_calculation.py
+++ grade_calculation.py
@@ -1,6 +1,6 @@
-mark = int(input("Enter marks"))
+#mark = int(input("Enter marks"))
 
-if mark >= 80:
+'''if mark >= 80:
     print("The Grade is A+")
 elif mark >= 70:
     print("The Grade is A")
@@ -14,6 +14,17 @@ elif mark >= 33:
     print("The Grade is D")
 
 else:
-    print(f"Failed Best of Luck Next Time {mark}")
+    print(f"Failed Best of Luck Next Time {mark}")'''
+mark = int(input("Enter Marks "))
+if 80 <= mark <= 100:
+    print("GPA is 5.00")
+elif 70 <= mark <= 79:
+    print("GPA is 4.00")
+elif 60 <= mark <= 69:
+    print("GPA is 3.00")
+
+
+
+
 
 print("Thanks for enter marks")
