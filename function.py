50e88b7cab2a0ba405dc559ae38c717885baf645
sharifdujee <sharifdu44@gmail.com>
2025-11-26 22:13:37 +0600

    use of function

---
+++ function.py
@@ -0,0 +1,13 @@
+def summation(a, b):
+    sum = a + b
+
+    print(sum)
+
+
+def substract(a, b):
+    sub = a - b;
+    print(sub)
+
+
+summation(10, 15)
+substract(10, 2)
---
+++ set.py
@@ -0,0 +1,7 @@
+num = {
+    1, 2, 3, 4, 5
+}
+num2 = set([1, 3, 8, 7])
+print(num | num2)
+print(num & num2)
+print(num - num2)
