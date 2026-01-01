862be7feb44c8259fe2e0ff59cdc04762ae1c4bd
sharifdujee <sharifdu44@gmail.com>
2026-01-01 22:57:23 +0600

    add label

--- test_tk.py
+++ test_tk.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 root = tk.Tk()
 message = tk.Label(root, text="Happy New Year, Robayet Bin Rahat")
@@ -8,6 +9,8 @@ root.title("Python GUI Interface")
 root.geometry('600x400+50+50')
 root.resizable(False, False)
 root.iconbitmap('./asset/pythontutorial-1-150x150.ico')
+tk.Label(root, text="classic Label").pack()
+ttk.Label(root, text="Themed Label").pack()
 
 
 root.mainloop()
