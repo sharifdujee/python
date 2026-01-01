cbd8e62d5d7678fb0de76eb32af57d3c8ef0028a
sharifdujee <sharifdu44@gmail.com>
2026-01-01 22:40:57 +0600

    window

--- test_tk.py
+++ test_tk.py
@@ -4,5 +4,10 @@ root = tk.Tk()
 message = tk.Label(root, text="Happy New Year, Robayet Bin Rahat")
 
 message.pack()
+root.title("Python GUI Interface")
+root.geometry('600x400+50+50')
+root.resizable(False, False)
+root.iconbitmap('./asset/pythontutorial-1-150x150.ico')
+
 
 root.mainloop()
