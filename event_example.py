7532c6adbc8b3a9d965045cdd2e7303c91ac8f62
sharifdujee <sharifdu44@gmail.com>
2026-01-06 22:53:06 +0600

    explore button

--- event_example.py
+++ event_example.py
@@ -3,13 +3,19 @@ from tkinter import ttk
 
 root = tk.Tk()
 
-def button_clicked():
+'''def button_clicked():
     print("Clikced the Button")
 
 
 button = ttk.Button(root, text="Click Me", command= button_clicked)
-button.pack()
+button.pack()'''
 
+# argument inside function
+def select(option):
+    print(option)
 
+ttk.Button(root, text="Rock", command=lambda:select("Rock")).pack()
+ttk.Button(root, text="Pack ", command=lambda:select("Pack")).pack()
+ttk.Button(root, text="Scissors", command=lambda:select("Scissors")).pack()
 
 root.mainloop()
